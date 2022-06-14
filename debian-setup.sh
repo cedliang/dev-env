45ffd4256a4dd2c78a00a8a911fe089e34170c6f
Cedric Liang <62864170+cedliang@users.noreply.github.com>
2022-06-14 16:41:08 +1000

    Create debian-setup.sh

---
+++ debian-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt install fd-find fzf fonts-firacode htop keychain openssh-client openssh-server tmux bat
+
+mkdir -p ~/.local/bin
+
+echo "export FZF_DEFAULT_COMMAND='fd'" >> ~/.bashrc
+echo "export FZF_DIRECTORIES_COMMAND='fd --type d'" >> ~/.bashrc
+echo 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' >> ~/.bashrc
+echo 'export FZF_ALT_C_COMMAND="$FZF_DIRECTORIES_COMMAND"' >> ~/.bashrc
+
+echo 'source /usr/share/doc/fzf/examples/key-bindings.bash' >> ~/.bashrc
+echo 'export PATH="~/.local/bin:$PATH"' >> ~/.bashrc
+
+ln -s $(which batcat) ~/.local/bin/bat
+ln -s $(which fdfind) ~/.local/bin/fd
