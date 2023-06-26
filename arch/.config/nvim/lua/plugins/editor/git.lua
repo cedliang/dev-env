27914b42f3f55aa3de65a73933bac4c17a836e9d
cedliang <me@cedric.app>
2023-06-26 14:02:23 +1000

    bump

--- arch/.config/nvim/lua/plugins/editor/git.lua
+++ arch/.config/nvim/lua/plugins/editor/git.lua
@@ -1,5 +1,6 @@
 vim.g.gitblame_delay = 250
 vim.g.gitblame_virtual_text_column = 120
+vim.g.gitblame_message_template = "<summary> • <date> • <author> • <sha>"
 return {
     { "f-person/git-blame.nvim", event = "BufReadPre" },
     {
