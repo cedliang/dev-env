06d32601c6b1358530d865fcc1986f5afded319f
cedliang <62864170+cedliang@users.noreply.github.com>
2023-07-15 00:11:49 +1000

    labels

--- arch/.config/nvim/lua/plugins/code/flash.lua
+++ arch/.config/nvim/lua/plugins/code/flash.lua
@@ -1,7 +1,15 @@
 return {
     "folke/flash.nvim",
-  -- stylua: ignore
-  keys = {
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search({ remote_op = { motion = true } }) end, desc = "Treesitter Search" },
-  },
+    ---@type Flash.Config
+    opts = {
+        modes = {
+            char = {
+                jump_labels = true,
+            },
+        },
+    },
+    -- stylua: ignore
+    keys = {
+        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search({ remote_op = { motion = true } }) end, desc = "Treesitter Search" },
+    },
 }
