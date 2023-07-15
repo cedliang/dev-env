c20c497cbd9c0154151704b8fb78f4d8d3e8b0db
cedliang <62864170+cedliang@users.noreply.github.com>
2023-07-15 13:24:42 +1000

    better flash config

--- arch/.config/nvim/lua/plugins/code/flash.lua
+++ arch/.config/nvim/lua/plugins/code/flash.lua
@@ -2,14 +2,12 @@ return {
     "folke/flash.nvim",
     ---@type Flash.Config
     opts = {
-        config = function(_)
-            vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#39480c", bg = "#bef129", bold = true })
-        end,
         modes = {
             char = {
                 jump_labels = true,
                 highlight = { backdrop = false },
                 autohide = true,
+                label = { current = false },
             },
             treesitter_search = {
                 label = { rainbow = { enabled = true } },
