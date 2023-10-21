051365d246fae0a2bfb63a267047068ba2b12b73
cedliang <62864170+cedliang@users.noreply.github.com>
2023-10-21 12:48:03 +1100

    better cpplua

--- arch/.config/nvim/lua/plugins/lang/cpp.lua
+++ arch/.config/nvim/lua/plugins/lang/cpp.lua
@@ -9,15 +9,10 @@ return {
         "neovim/nvim-lspconfig",
         opts = {
             servers = {
-                clangd = { mason = false },
-            },
-            setup = {
-                clangd = function(_, opts)
-                    local capabilities = vim.lsp.protocol.make_client_capabilities()
-                    capabilities.offsetEncoding = { "utf-16" }
-                    require("lspconfig").clangd.setup({ capabilities = capabilities })
-                    return true
-                end,
+                clangd = {
+                    mason = false,
+                    capabilities = { offsetEncoding = { "utf-16" } },
+                },
             },
         },
     },
