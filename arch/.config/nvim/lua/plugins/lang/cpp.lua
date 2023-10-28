57c0b5a705aa3e1926a9aca63e0817f2c439fcd0
cedliang <62864170+cedliang@users.noreply.github.com>
2023-10-28 15:32:22 +1100

    clangd

--- arch/.config/nvim/lua/plugins/lang/cpp.lua
+++ arch/.config/nvim/lua/plugins/lang/cpp.lua
@@ -1,5 +1,35 @@
 return {
     { import = "lazyvim.plugins.extras.lang.clangd" },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "p00f/clangd_extensions.nvim",
+                inlay_hints = {
+                    inline = vim.fn.has("nvim-0.10") == 1,
+                },
+            },
+        },
+        opts = {
+            servers = {
+                clangd = {
+                    cmd = {
+                        "clangd",
+                        "--background-index",
+                        "--clang-tidy=0",
+                        "--header-insertion=iwyu",
+                        "--completion-style=detailed",
+                        "--function-arg-placeholders",
+                        "--fallback-style=llvm",
+                    },
+                    on_attach = function(client, _)
+                        require("clangd_extensions.inlay_hints").setup_autocmd()
+                        require("clangd_extensions.inlay_hints").set_inlay_hints()
+                    end,
+                },
+            },
+        },
+    },
     -- {
     --     "nvim-treesitter/nvim-treesitter",
     --     opts = function(_, opts)
