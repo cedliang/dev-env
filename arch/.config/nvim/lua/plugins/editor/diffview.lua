3627ad7f93c65da116c7b27bd2de847f58500d9c
cedliang <62864170+cedliang@users.noreply.github.com>
2023-11-23 22:12:05 +1100

    git diff

---
+++ arch/.config/nvim/lua/plugins/editor/diffview.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "sindrets/diffview.nvim",
+        keys = {
+            { "<leader>gdo", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+            { "<leader>gdf", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview File History" },
+            { "<leader>gdb", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview Branch History" },
+            { "<leader>gdc", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+        },
+    },
+}
--- arch/.config/nvim/lua/plugins/lang/csharp.lua
+++ arch/.config/nvim/lua/plugins/lang/csharp.lua
@@ -1,7 +1,7 @@
 local util = require("lspconfig.util")
 
 local root_dir_func
-local charp_ls_on_attach_func
+local csharp_ls_on_attach_func
 local omnisharp_on_attach_func
 local omnisharp_on_new_config
 local omnisharp_mono_func
@@ -22,7 +22,7 @@ local function return_main(mono)
         {
             "nvim-treesitter/nvim-treesitter",
             opts = function(_, opts)
-                table.insert(opts.ensure_installed, "c_sharp")
+                table.insert(opts.ensure_installed, { "xml", "c_sharp" })
             end,
         },
         {
@@ -57,7 +57,7 @@ root_dir_func = function(fname)
     return util.root_pattern("*.sln")(fname) or util.root_pattern("*.csproj")(fname)
 end
 
-charp_ls_on_attach_func = function(client, _)
+csharp_ls_on_attach_func = function(client, _)
     -- lsp keyword breaks since it designates too much as a keyword (ie, conditionals, etc)
     -- here we let it fallback to the default highlight group
     vim.api.nvim_set_hl(0, "@lsp.type.keyword", {})
@@ -105,81 +105,6 @@ omnisharp_on_attach_func = function(client, _)
         "gd",
         "<cmd>lua require('omnisharp_extended').telescope_lsp_definitions()<cr>",
         desc = "Goto Definition (omnisharp)",
-    }
-    vim.api.nvim_set_hl(0, "@lsp.type.keyword", {})
-    client.server_capabilities.semanticTokensProvider = {
-        full = vim.empty_dict(),
-        legend = {
-            tokenModifiers = { "static_symbol" },
-            tokenTypes = {
-                "comment",
-                "excluded_code",
-                "identifier",
-                "keyword",
-                "keyword_control",
-                "number",
-                "operator",
-                "operator_overloaded",
-                "preprocessor_keyword",
-                "string",
-                "whitespace",
-                "text",
-                "static_symbol",
-                "preprocessor_text",
-                "punctuation",
-                "string_verbatim",
-                "string_escape_character",
-                "class_name",
-                "delegate_name",
-                "enum_name",
-                "interface_name",
-                "module_name",
-                "struct_name",
-                "type_parameter_name",
-                "field_name",
-                "enum_member_name",
-                "constant_name",
-                "local_name",
-                "parameter_name",
-                "method_name",
-                "extension_method_name",
-                "property_name",
-                "event_name",
-                "namespace_name",
-                "label_name",
-                "xml_doc_comment_attribute_name",
-                "xml_doc_comment_attribute_quotes",
-                "xml_doc_comment_attribute_value",
-                "xml_doc_comment_cdata_section",
-                "xml_doc_comment_comment",
-                "xml_doc_comment_delimiter",
-                "xml_doc_comment_entity_reference",
-                "xml_doc_comment_name",
-                "xml_doc_comment_processing_instruction",
-                "xml_doc_comment_text",
-                "xml_literal_attribute_name",
-                "xml_literal_attribute_quotes",
-                "xml_literal_attribute_value",
-                "xml_literal_cdata_section",
-                "xml_literal_comment",
-                "xml_literal_delimiter",
-                "xml_literal_embedded_expression",
-                "xml_literal_entity_reference",
-                "xml_literal_name",
-                "xml_literal_processing_instruction",
-                "xml_literal_text",
-                "regex_comment",
-                "regex_character_class",
-                "regex_anchor",
-                "regex_quantifier",
-                "regex_grouping",
-                "regex_alternation",
-                "regex_text",
-                "regex_self_escaped_character",
-                "regex_other_escape",
-            },
-        },
-        range = true,
     }
 end
 
@@ -268,7 +193,7 @@ end
 
 csharp_ls_func = function(_, opts)
     require("lspconfig").csharp_ls.setup({
-        on_attach = charp_ls_on_attach_func,
+        on_attach = csharp_ls_on_attach_func,
         handlers = {
             ["textDocument/publishDiagnostics"] = function(_, _, _, _) end,
         },
