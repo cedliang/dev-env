90cae09197a792c8b112809cd1fa373145751a42
cedliang <62864170+cedliang@users.noreply.github.com>
2024-07-15 10:36:53 +1000

    osc52

--- arch/.config/nvim/lua/config/options.lua
+++ arch/.config/nvim/lua/config/options.lua
@@ -8,3 +8,22 @@ opt.softtabstop = 4
 opt.shiftwidth = 4
 
 opt.clipboard = "unnamedplus"
+-- -- uncomment to force clipboard to use OSC 52
+-- -- useful on headless remotes
+-- local function paste()
+--     return {
+--         vim.fn.split(vim.fn.getreg(""), "\n"),
+--         vim.fn.getregtype(""),
+--     }
+-- end
+-- vim.g.clipboard = {
+--     name = "OSC 52",
+--     copy = {
+--         ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+--         ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+--     },
+--     paste = {
+--         ["+"] = paste,
+--         ["*"] = paste,
+--     },
+-- }
