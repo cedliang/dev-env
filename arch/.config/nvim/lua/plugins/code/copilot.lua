3d67ca29933f9f294f5e3297c7c2c0a367014070
cedliang <62864170+cedliang@users.noreply.github.com>
2024-11-19 15:52:54 +1100

    ai

--- arch/.config/nvim/lua/plugins/code/copilot.lua
+++ arch/.config/nvim/lua/plugins/code/copilot.lua
@@ -1,6 +1,6 @@
 return {
-    { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     -- {
     --     "hrsh7th/nvim-cmp",
     --     opts = function(_, opts)
