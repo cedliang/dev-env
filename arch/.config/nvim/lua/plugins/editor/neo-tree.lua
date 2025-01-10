8c73b27bd8f34a024560d7a6ce63faf32d45277e
cedliang <62864170+cedliang@users.noreply.github.com>
2025-01-10 15:52:36 +1100

    doc symbols

--- arch/.config/nvim/lua/plugins/editor/neo-tree.lua
+++ arch/.config/nvim/lua/plugins/editor/neo-tree.lua
@@ -2,6 +2,12 @@ return {
     {
         "nvim-neo-tree/neo-tree.nvim",
         opts = {
+            sources = {
+                "filesystem",
+                "buffers",
+                "git_status",
+                "document_symbols",
+            },
             source_selector = {
                 winbar = true,
                 content_layout = "center",
